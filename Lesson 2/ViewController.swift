5379135a0af00fbe2684f988a4d3d29a89046d0f
Pavel Levin <uxwithlove@gmail.com>
2020-02-02 20:29:27 +0300

    Initial Commit

---
+++ Lesson 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lesson 2
+//
+//  Created by Pavel Levin on 02/02/2020.
+//  Copyright © 2020 Pavel Levin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
